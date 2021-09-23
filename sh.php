1866c90c1c4fac24bc9e8b60cbfed0165ae43d8c
Jim C K Flaten <jckf@jckf.no>
2021-09-24 00:32:21 +0200

    CLI functionality.

--- sh.php
+++ sh.php
@@ -2,4 +2,11 @@
 
 require('bootstrap/bootstrap.php');
 
+(function () {
+    if (method_exists($eaw = eaw(), $command = $_SERVER['argv'][1] ?? null)) {
+        echo json_encode(call_user_func_array([ $eaw, $command ], array_slice($_SERVER['argv'], 2)), JSON_PRETTY_PRINT);
+        exit;
+    }
+})();
+
 call_user_func(Psy\bin());
