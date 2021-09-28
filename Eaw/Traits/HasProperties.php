a9f0cd4f52ad4e3554d40a6ee70e3425755bdcce
Jim C K Flaten <jckf@jckf.no>
2021-09-28 22:05:21 +0200

    Fall back to reloading the model with properties if no controller is available.

--- Eaw/Traits/HasProperties.php
+++ Eaw/Traits/HasProperties.php
@@ -3,11 +3,33 @@
 namespace Eaw\Traits;
 
 use Eaw\Models\Property;
+use Eaw\QueryBuilder;
 
 trait HasProperties
 {
+    /**
+     * Get this model's properties.
+     *
+     * Returns a Paginator for models with their own property controller. Returns an array of Properties otherwise.
+     *
+     * @return QueryBuilder|Property[]
+     */
     public function properties()
     {
-        return $this->client->query($this->getFullPath() . '/properties')->setModel(Property::class);
+        if ($this->hasPropertyController ?? true) {
+            return $this->client->query($this->getFullPath() . '/properties')->setModel(Property::class);
+        }
+
+        if (!is_array($this->getAttribute('properties'))) {
+            $withProperties = static::newQuery($this->getPath())
+                ->with([ 'properties' ])
+                ->get($this->getKey());
+
+            $this->attributes['properties'] = $withProperties->getAttribute('properties') ?? [];
+        }
+
+        return array_map(function (array $attributes) {
+            return Property::newInstance($attributes);
+        }, $this->getAttribute('properties'));
     }
 }
