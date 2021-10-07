35506cf03227293895883ab96341b4b67ce06d8d
Jim C K Flaten <jckf@jckf.no>
2021-10-07 16:42:52 +0200

    Better presentation of Models in the shell.

--- sh.php
+++ sh.php
@@ -1,7 +1,13 @@
 <?php
 
+use Eaw\Models\Model;
+use Psy\Configuration;
+use Psy\Shell;
+use Symfony\Component\VarDumper\Caster\Caster;
+
 require(__DIR__ . '/bootstrap/bootstrap.php');
 
+// TODO: Move this to Psy commands?
 (function () {
     if (method_exists($eaw = eaw(), $command = $_SERVER['argv'][1] ?? null)) {
         echo json_encode(call_user_func_array([ $eaw, $command ], array_slice($_SERVER['argv'], 2)), JSON_PRETTY_PRINT);
@@ -9,4 +15,19 @@ require(__DIR__ . '/bootstrap/bootstrap.php');
     }
 })();
 
-call_user_func(Psy\bin());
+$config = new Configuration();
+$config->getPresenter()->addCasters([
+    Model::class => function (Model $model) {
+        $result = [];
+
+        foreach ($model->getAttributes() as $key => $value) {
+            $result[Caster::PREFIX_VIRTUAL . $key] = $value;
+        }
+
+        return $result;
+    }
+]);
+
+$shell = new Shell($config);
+$shell->setIncludes($_SERVER['argv']);
+$shell->run();
