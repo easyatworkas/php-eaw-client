f6c7c6c164ea2b81bddf732297d98a5ed9f30683
Jim C K Flaten <jckf@jckf.no>
2021-10-07 16:52:26 +0200

    Add customers relation to setting group.

--- Eaw/Models/SettingGroup.php
+++ Eaw/Models/SettingGroup.php
@@ -9,4 +9,9 @@ class SettingGroup extends Model
     use HasProperties;
 
     protected $path = '/setting_groups';
+
+    public function customers()
+    {
+        return $this->client->query($this->getFullPath() . '/members')->setModel(Customer::class);
+    }
 }
