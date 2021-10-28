8aab302a8f7c78bf57be19ecb61d75f860241ec2
Jim C K Flaten <jckf@jckf.no>
2021-10-28 03:45:57 +0200

    A few more tests.

--- spec/shared/has-attributes.php
+++ spec/shared/has-attributes.php
@@ -2,7 +2,11 @@
 
 describe('behavior from HasAttributes trait', function () {
     describe('->setAttributes()', function () {
+        it('should take an array of attributes', function () {
+            $this->instance->setAttributes($this->attributes);
 
+            assert(true);
+        });
     });
 
     describe('->getAttributes()', function () {
@@ -14,19 +18,63 @@ describe('behavior from HasAttributes trait', function () {
     });
 
     describe('->hasAttribute()', function () {
+        it('returns true for known attributes', function () {
+            $this->instance->setAttributes([ 'foo' => 'bar' ]);
 
+            assert($this->instance->hasAttribute('foo'));
+        });
+
+        it('returns false for unknown attributes', function () {
+            $this->instance->setAttributes([]);
+
+            assert(!$this->instance->hasAttribute('foo'));
+        });
     });
 
     describe('->setAttribute()', function () {
+        it('should take an attribute and a value', function () {
+            $this->instance->setAttributes([]);
 
+            foreach ($this->attributes as $attribute => $value) {
+                $this->instance->setAttribute($attribute, $value);
+            }
+
+            assert(true);
+        });
     });
 
     describe('->getAttribute()', function () {
+        it('should return value of known attributes', function () {
+            foreach ($this->attributes as $attribute => $value) {
+                assert($this->instance->getAttribute($attribute) === $value);
+            }
+        });
 
+        it('should return null for unknown attributes', function () {
+            $this->instance->setAttributes([]);
+
+            assert($this->instance->getAttribute('foo') === null);
+        });
     });
 
     describe('->unsetAttribute()', function () {
+        it('should unset known attributes', function () {
+            foreach ($this->attributes as $attribute => $value) {
+                assert($this->instance->getAttribute($attribute) === $value);
 
+                $this->instance->unsetAttribute($attribute);
+
+                assert($this->instance->getAttribute($attribute) === null);
+            }
+        });
+
+        it('should accept unknown attributes', function () {
+            $this->instance->setAttributes([]);
+
+            $this->instance->unsetAttribute('foo');
+
+            assert(true);
+        });
     });
 
     describe('->syncOriginal()', function () {
