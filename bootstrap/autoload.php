bb46a536749c2666c5388f71c56482f88b8656d4
Jim C K Flaten <jckf@jckf.no>
2022-05-25 14:50:54 +0200

    We obviously only run Windows (:

--- bootstrap/autoload.php
+++ bootstrap/autoload.php
@@ -3,7 +3,7 @@
 require(__DIR__ . '/../vendor/autoload.php');
 
 spl_autoload_register(function ($class) {
-    if (file_exists($file = __DIR__ . '/../' . $class . '.php')) {
+    if (file_exists($file = __DIR__ . '/../' . str_replace('\\', '/', $class) . '.php')) {
         require($file);
     }
 });
