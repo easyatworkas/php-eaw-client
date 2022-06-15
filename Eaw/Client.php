fb327dabcd827fa1e1983ba53bff4c90081100d7
Jim C K Flaten <jckf@jckf.no>
2022-06-15 19:32:47 +0200

    Move more logic out of Client.

--- Eaw/Client.php
+++ Eaw/Client.php
@@ -3,6 +3,7 @@
 namespace Eaw;
 
 use Eaw\Traits\AuthenticatesClient;
+use Eaw\Traits\BuildsHttpRequestData;
 use Eaw\Traits\MakesCrudRequests;
 use Eaw\Traits\IsSingleton;
 use GuzzleHttp\Client as Guzzle;
@@ -15,6 +16,7 @@ use Psr\Http\Message\ResponseInterface;
 class Client
 {
     use IsSingleton;
+    use BuildsHttpRequestData;
     use MakesCrudRequests;
     use AuthenticatesClient;
 
@@ -80,68 +82,6 @@ class Client
     public function execute()
     {
         $this->handler->execute();
-    }
-
-    /**
-     * @param string $path
-     * @param array|null $parameters
-     * @return string
-     */
-    protected function buildRequestUrl(string $path = '/', array $parameters = null): string
-    {
-        $url = $this->baseUrl . $path;
-
-        if ($parameters) {
-            $url .= '?' . http_build_query($parameters);
-        }
-
-        return $url;
-    }
-
-    /**
-     * @param array|null $data
-     * @param array|null $files
-     * @return array
-     */
-    protected function buildRequestOptions(array $data = null, array $files = null): array
-    {
-        $options = [
-            'headers' => $this->headers,
-            'multipart' => [],
-        ];
-
-        if ($files) {
-            if ($data) {
-                // Super hacky way to build a multipart request.
-                foreach (explode('&', http_build_query($data)) as $pair) {
-                    list($var, $val) = explode('=', $pair, 2);
-
-                    $options['multipart'][] = [
-                        'name' => urldecode($var),
-                        'contents' => urldecode($val),
-                    ];
-                }
-            }
-
-            foreach ($files as $name => $file) {
-                if (is_resource($file)) {
-                    $file = [
-                        'handle' => $file,
-                        'filename' => stream_get_meta_data($file)['uri'] ?? null,
-                    ];
-                }
-
-                $options['multipart'][] = array_filter([
-                    'name' => $name,
-                    'contents' => $file['handle'] ?? null,
-                    'filename' => $file['filename'] ?? null,
-                ]);
-            }
-        } else {
-            $options['json'] = $data;
-        }
-
-        return array_filter($options);
     }
 
     /**
---
+++ Eaw/Traits/BuildsHttpRequestData.php
@@ -0,0 +1,72 @@
+<?php
+
+namespace Eaw\Traits;
+
+/**
+ * @property string $baseUrl Defined in Client.
+ * @property array $headers Defined in Client.
+ */
+trait BuildsHttpRequestData
+{
+    /**
+     * @param string $path
+     * @param array|null $parameters
+     * @return string
+     */
+    protected function buildRequestUrl(string $path = '/', array $parameters = null): string
+    {
+        $url = $this->baseUrl . $path;
+
+        if ($parameters) {
+            $url .= '?' . http_build_query($parameters);
+        }
+
+        return $url;
+    }
+
+    /**
+     * @param array|null $data
+     * @param array|null $files
+     * @return array
+     */
+    protected function buildRequestOptions(array $data = null, array $files = null): array
+    {
+        $options = [
+            'headers' => $this->headers,
+            'multipart' => [],
+        ];
+
+        if ($files) {
+            if ($data) {
+                // Super hacky way to build a multipart request.
+                foreach (explode('&', http_build_query($data)) as $pair) {
+                    list($var, $val) = explode('=', $pair, 2);
+
+                    $options['multipart'][] = [
+                        'name' => urldecode($var),
+                        'contents' => urldecode($val),
+                    ];
+                }
+            }
+
+            foreach ($files as $name => $file) {
+                if (is_resource($file)) {
+                    $file = [
+                        'handle' => $file,
+                        'filename' => stream_get_meta_data($file)['uri'] ?? null,
+                    ];
+                }
+
+                $options['multipart'][] = array_filter([
+                    'name' => $name,
+                    'contents' => $file['handle'] ?? null,
+                    'filename' => $file['filename'] ?? null,
+                ]);
+            }
+        } else {
+            $options['json'] = $data;
+        }
+
+        return array_filter($options);
+    }
+}
