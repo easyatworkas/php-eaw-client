51e4768ac9d27b5914efe5678b0d579504da0f52
Jim C K Flaten <jckf@jckf.no>
2024-07-12 15:55:36 +0200

    Complete output with a newline.

--- eaw.php
+++ eaw.php
@@ -21,7 +21,7 @@ if (file_exists($autoexec = '.eaw_autoexec.php')) {
 // TODO: Move this to Psy commands?
 (function () {
     if (method_exists($eaw = eaw(), $command = $_SERVER['argv'][1] ?? null)) {
-        echo json_encode(call_user_func_array([ $eaw, $command ], array_slice($_SERVER['argv'], 2)), JSON_PRETTY_PRINT);
+        echo json_encode(call_user_func_array([ $eaw, $command ], array_slice($_SERVER['argv'], 2)), JSON_PRETTY_PRINT), PHP_EOL;
         exit;
     }
 })();
